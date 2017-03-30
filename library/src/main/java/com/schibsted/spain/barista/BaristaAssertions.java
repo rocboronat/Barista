67e149d75241ef2e472da7e5d0600a1165e24a09
Roc Boronat <roc@fewlaps.com>
2017-03-30 09:15:13 +0200

    Remove an unneeded parenthesis

--- library/src/main/java/com/schibsted/spain/barista/BaristaAssertions.java
+++ library/src/main/java/com/schibsted/spain/barista/BaristaAssertions.java
@@ -36,7 +36,7 @@ public class BaristaAssertions {
   }
 
   public static void assertDisplayed(String text) {
-    onView((firstViewOf(allOf(withText(text), isDisplayed())))).check(matches(isDisplayed()));
+    onView(firstViewOf(allOf(withText(text), isDisplayed()))).check(matches(isDisplayed()));
   }
 
   public static void assertNotExist(int id) {
